0b390a9b4e004cb5517269536a8d8e3820df9683
vzvu3k6k <vzvu3k6k@gmail.com>
2024-03-15 01:17:07 +0900

    wip

--- cmd/hyper-paper/main.go
+++ cmd/hyper-paper/main.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"math"
 	"os"
 	"strconv"
 )
@@ -26,33 +27,46 @@ func lookupAttrValue(attrs []xml.Attr, name string) (string, bool) {
 // handlePageElementはPAGE要素の属性を処理する。
 // PAGE要素のWIDTHとHEIGHT属性をそれぞれ返す。属性が存在しない場合、エラーを返す。
 func handlePageElement(element xml.StartElement) (int, int, error) {
-	var pageWidth, pageHeight int
-	var err error
-
-	for _, attr := range element.Attr {
-		switch attr.Name.Local {
-		case "WIDTH":
-			pageWidth, err = strconv.Atoi(attr.Value)
-			if err != nil {
-				return 0, 0, err
-			}
-		case "HEIGHT":
-			pageHeight, err = strconv.Atoi(attr.Value)
-			if err != nil {
-				return 0, 0, err
-			}
-		}
+	pageWidthValue, ok := lookupAttrValue(element.Attr, "WIDTH")
+	if !ok {
+		return 0, 0, fmt.Errorf("page width is not set")
+	}
+	pageHeightValue, ok := lookupAttrValue(element.Attr, "HEIGHT")
+	if !ok {
+		return 0, 0, fmt.Errorf("page height is not set")
 	}
 
-	// WIDTHとHEIGHTが存在しない場合は0のままなのでエラーを返す。
-	if pageWidth == 0 || pageHeight == 0 {
-		return 0, 0, fmt.Errorf("page width or height is not set")
+	pageWidth, err := strconv.Atoi(pageWidthValue)
+	if err != nil {
+		return 0, 0, err
+	}
+	pageHeight, err := strconv.Atoi(pageHeightValue)
+	if err != nil {
+		return 0, 0, err
 	}
 
 	return pageWidth, pageHeight, nil
 }
 
-func buildHTML(in io.Reader) error {
+func almostEqual(a, b float64) bool {
+	const epsilon = 1e-3 * 5
+	return math.Abs(a-b) < epsilon
+}
+
+func lookupBoundingBox(boxes []*BoundingBox, page int, x, y, width, height float64) (*BoundingBox, bool) {
+	for _, box := range boxes {
+		if box.Page == page &&
+			almostEqual(box.X, x) &&
+			almostEqual(box.Y, y) &&
+			almostEqual(box.Width, width) &&
+			almostEqual(box.Height, height) {
+			return box, true
+		}
+	}
+	return nil, false
+}
+
+func buildHTML(in io.Reader, boundingBoxes []*BoundingBox) error {
 	var pageWidth, pageHeight int
 
 	d := xml.NewDecoder(in)
@@ -74,7 +88,66 @@ func buildHTML(in io.Reader) error {
 					return err
 				}
 			case "LINE":
+				typeAttr, ok := lookupAttrValue(token.Attr, "TYPE")
+				if !ok {
+					return fmt.Errorf("LINE element has no TYPE attribute")
+				}
+				if typeAttr == "本文" {
+					stringAttr, ok := lookupAttrValue(token.Attr, "STRING")
+					if !ok {
+						return fmt.Errorf("LINE element has no STRING attribute")
+					}
+					_ = stringAttr
+				}
 
+				xAttrValue, ok := lookupAttrValue(token.Attr, "X")
+				if !ok {
+					return fmt.Errorf("LINE element has no X attribute")
+				}
+				x, err := strconv.Atoi(xAttrValue)
+				if err != nil {
+					return err
+				}
+
+				yAttrValue, ok := lookupAttrValue(token.Attr, "Y")
+				if !ok {
+					return fmt.Errorf("LINE element has no Y attribute")
+				}
+				y, err := strconv.Atoi(yAttrValue)
+				if err != nil {
+					return err
+				}
+
+				widthAttrValue, ok := lookupAttrValue(token.Attr, "WIDTH")
+				if !ok {
+					return fmt.Errorf("LINE element has no WIDTH attribute")
+				}
+				width, err := strconv.Atoi(widthAttrValue)
+				if err != nil {
+					return err
+				}
+
+				heightAttrValue, ok := lookupAttrValue(token.Attr, "HEIGHT")
+				if !ok {
+					return fmt.Errorf("LINE element has no HEIGHT attribute")
+				}
+				height, err := strconv.Atoi(heightAttrValue)
+				if err != nil {
+					return err
+				}
+
+				boundingBox, ok := lookupBoundingBox(
+					boundingBoxes,
+					1,
+					float64(x)/float64(pageWidth),
+					float64(y)/float64(pageHeight),
+					float64(width)/float64(pageWidth),
+					float64(height)/float64(pageHeight),
+				)
+				if !ok {
+					return fmt.Errorf("bounding box for %q not found", token)
+				}
+				fmt.Printf("bb: %+v\n", boundingBox.Text)
 			}
 		case xml.EndElement:
 			//do something
@@ -95,7 +168,7 @@ func buildHTML(in io.Reader) error {
 }
 
 type BoundingBox struct {
-	Page   float64
+	Page   int
 	X      float64
 	Y      float64
 	Width  float64
@@ -116,6 +189,26 @@ func loadBoundingBoxes(in io.Reader) ([]*BoundingBox, error) {
 		return nil, err
 	}
 
+	// 最初の行でページサイズを取得する。
+	record, err := r.Read()
+	if err != nil {
+		return nil, err
+	}
+	if record[11] != "###PAGE###" {
+		return nil, fmt.Errorf("expected ###PAGE###, but got %s", record[11])
+	}
+	if record[10] != "-1" {
+		return nil, fmt.Errorf("expected conf is -1, but got %s", record[10])
+	}
+	pageWidth, err := strconv.ParseFloat(record[8], 64)
+	if err != nil {
+		return nil, err
+	}
+	pageHeight, err := strconv.ParseFloat(record[9], 64)
+	if err != nil {
+		return nil, err
+	}
+
 	// CSVの各行をBoundingBox構造体に詰め替える。
 	for {
 		record, err := r.Read()
@@ -126,7 +219,7 @@ func loadBoundingBoxes(in io.Reader) ([]*BoundingBox, error) {
 			return nil, err
 		}
 
-		page, err := strconv.ParseFloat(record[1], 64)
+		page, err := strconv.Atoi(record[1])
 		if err != nil {
 			return nil, err
 		}
@@ -150,10 +243,10 @@ func loadBoundingBoxes(in io.Reader) ([]*BoundingBox, error) {
 
 		boundingBoxes = append(boundingBoxes, &BoundingBox{
 			Page:   page,
-			X:      x,
-			Y:      y,
-			Width:  width,
-			Height: height,
+			X:      x / pageWidth,
+			Y:      y / pageHeight,
+			Width:  width / pageWidth,
+			Height: height / pageHeight,
 			Text:   text,
 		})
 	}
@@ -179,5 +272,7 @@ func main() {
 	}
 	defer layoutFile.Close()
 
-	buildHTML(layoutFile)
+	if err := buildHTML(layoutFile, boxes); err != nil {
+		handleErr(err)
+	}
 }
